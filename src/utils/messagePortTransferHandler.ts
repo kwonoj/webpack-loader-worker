50af9061d314f5bd0a0cfa6220a13facf7cbed2b
OJ Kwon <kwon.ohjoong@gmail.com>
2019-11-30 21:16:26 -0800

    fix(transferhandler): release proxy where possible

--- src/utils/messagePortTransferHandler.ts
+++ src/utils/messagePortTransferHandler.ts
@@ -1,14 +1,20 @@
-import { expose, proxyMarker, transferHandlers, wrap } from 'comlink';
+import { expose, proxyMarker, releaseProxy, transferHandlers, wrap } from 'comlink';
 
 import { MessageChannel } from 'worker_threads';
 
 const nodeEndpoint: Function = require('comlink/dist/umd/node-adapter');
 
+const proxiesSet = new Set<any>();
 /**
  * Override comlink's default proxy handler to use Node endpoints
  * https://github.com/GoogleChromeLabs/comlink/issues/313
  */
 const setupTransferHandler = () => {
+  for (const proxy of proxiesSet) {
+    proxy[releaseProxy]();
+  }
+  proxiesSet.clear();
+
   transferHandlers.set('proxy', {
     canHandle: (obj: object) => obj && obj[proxyMarker],
     serialize: (obj: object) => {
@@ -19,7 +25,9 @@ const setupTransferHandler = () => {
     deserialize: (port) => {
       port = nodeEndpoint(port);
       port.start();
-      return wrap(port);
+      const value = wrap(port);
+      proxiesSet.add(value);
+      return value;
     }
   });
 };
